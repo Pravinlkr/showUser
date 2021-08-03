a496660490f83e4d4144d9e5b1e2de437c6a4166
Pravinlkr <pravinlakhisarai@gmail.com>
2021-08-03 13:06:25 +0530

    design completed for user assignment, added form-model component to user component and designed header, footer and table for user component

---
+++ src/components/formModel.vue
@@ -0,0 +1,125 @@
+<template>
+<div class="model" v-if="showModel">
+    <div class="modal-content">
+        <div class="modelHeader">
+            <h3>Add New User</h3>
+            <button v-on:click="closeModel()">X</button>
+        </div>
+        <div class="modelBody">
+            <label for="email">Email :</label><br />
+            <input type="email" class="email" id="email" placeholder="Email"><br />
+
+            <label for="name">Full Name :</label><br />
+            <input type="text" class="name" id="name" placeholder="Full Name"><br />
+
+            <label for="dob">Date Of Birth :</label><br />
+            <input type="date" class="dob" id="dob" placeholder="Date Of Birth"><br />
+
+            <label for="password">Password :</label><br />
+            <input type="password" class="password" id="password" placeholder="Password"><br />
+
+            <label for="cpassword">Confirm Password :</label><br />
+            <input type="password" class="cpassword" id="cpassword" placeholder="Confirm Password"><br />
+            <input type="checkbox" class="termCondition" id="termCondition">
+            <label for="termCondition" class="termConditionLabel">I accept all terms and conditions.</label><br />
+            <button class="add">Add</button>
+        </div>
+    </div>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'formModel',
+    props:{
+        showModel:{
+            type:Boolean,
+            default:false
+        }
+    },
+    methods: {
+        closeModel() {
+            this.$emit('updateModel');
+        }
+    }
+}
+</script>
+
+<style scoped>
+.model {
+    position: fixed;
+    /* Stay in place */
+    z-index: 1;
+    overflow: auto;
+    /* Enable scroll if needed */
+    background-color: rgb(0, 0, 0);
+    /* Fallback color */
+    background-color: rgba(0, 0, 0, 0.4);
+    padding-top: 30px;
+    /* Location of the box */
+    left: 0;
+    top: 0;
+    width: 100%;
+    /* Full width */
+    height: 100%;
+}
+
+.modal-content {
+    background-color: #fefefe;
+    margin: auto;
+    padding: 10px;
+    border: 1px solid #888;
+    width: 50%;
+}
+
+.modelHeader {
+    height: 50px;
+    margin-bottom: 1%;
+    width: 100%;
+    border-bottom: 1px solid #ccc;
+}
+
+.modelHeader h3 {
+    margin: 2% 2%;
+    float: left;
+}
+
+.modelHeader button {
+    margin: 2% 2%;
+    float: right;
+}
+
+label {
+    font-size: 15px;
+    font-weight: 500;
+    margin: 2% 25%;
+}
+
+input {
+    border: 1px solid #ccc;
+    height: 30px;
+    margin: 2% 25%;
+    width: 50%;
+    outline:none;
+}
+
+label.termConditionLabel {
+    margin: 2% 1%;
+}
+
+input.termCondition {
+    width: 15px;
+    height: 15px;
+    margin-right: 1%;
+}
+
+button.add {
+    height: 30px;
+    background-color: green;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    width: 80px;
+    margin: 2% 45%;
+}
+</style>
--- src/components/user.vue
+++ src/components/user.vue
@@ -1,11 +1,140 @@
 <template>
-    <p>this is user component</p>
+<div class="header">
+    <h2>User Details</h2>
+    <button class="add" v-on:click="showModel()">Add New User</button>
+</div>
+<div class="tableBox">
+    <table>
+        <tr>
+            <th>Full Name</th>
+            <th>Email</th>
+            <th>Date Of Birth</th>
+            <th>Password</th>
+            <th>Edit</th>
+            <th>Delete</th>
+        </tr>
+        <tr v-for="(user,index) in userList" v-bind:key="index">
+            <td>{{user.name}}</td>
+            <td>{{user.email}}</td>
+            <td>{{user.dob}}</td>
+            <td>{{user.password}}</td>
+            <td>
+                <button class="edit">Edit</button>
+            </td>
+            <td>
+                <button class="delete">Delete</button>
+            </td>
+        </tr>
+    </table>
+</div>
+<div class="footer">
+    <input type="checkbox" class="sortCheckbox" id="sortCheckbox">
+    <label for="sortCheckbox">Sort By Date Of Birth</label>
+</div>
+<form-model :showModel="showModelValue" @updateModel="updateModel"></form-model>
 </template>
+
 <script>
-export default{
-    name:'user'
+import formModel from './formModel.vue'
+export default {
+    name: 'user',
+    components: {
+        formModel
+    },
+    data(){
+        return{
+            showModelValue:false,
+            userList:[
+                {name:'Manish Singh', email:'manishsingh@gmail.com', dob:'21-01-1995', password:'mainsh123'},
+                {name:'Vikkey Kumar', email:'kumarVikkey@gmail.com', dob:'02-03-1993', password:'vikkey123'},
+                {name:'Pradeep Yadav', email:'pradeep@gmail.com', dob:'21-01-1997', password:'pradeep007'},
+                {name:'Abhishek Ranjan', email:'arj@gmail.com', dob:'21-01-1998', password:'arj@1234'},
+                {name:'Ankit Kumar', email:'akumar001@gmail.com', dob:'21-01-1996', password:'akumar001'},
+                ]
+        }
+    },
+    methods:{
+        showModel(){
+            console.log("called");
+            this.showModelValue = true;
+            console.log(this.showModelValue);
+        },
+        updateModel(){
+            console.log(this.showModelValue);
+            this.showModelValue = false;
+        }
+    }
 }
 </script>
-<style scoped>
 
+<style scoped>
+.header{
+    width:80%;
+    height: 70px;
+    margin:0 auto;
+}
+.header h2{
+    margin:1% 2%;
+    float: left;
+}
+button.add {
+    height: 30px;
+    margin:1% 2%;
+    background-color: green;
+    border: none;
+    border-radius: 5px;
+    color: white;
+    padding:5px 10 px;
+    float:right;
+}
+.tableBox{
+    width: 80%;
+    height: auto;
+    margin:0 auto;
+    text-align: center;
+}
+table{
+    width: 100%;
+}
+table th{
+    background-color: #303030;
+    color:white;
+    padding:10px 10px;
+}
+table td{
+    padding-top: 10px;
+    font-size: 16px;
+}
+button.edit{
+    border:none;
+    outline: none;
+    color:white;
+    background-color: #42a3ed;
+    height: 30px;
+    width: 70%;
+}
+button.delete{
+    border:none;
+    outline: none;
+    color:white;
+    background-color: #ff0d39;
+    height: 30px;
+    width: 70%;
+}
+.footer{
+    height: 50px;
+    background-color: #ccc;
+    width: 100%;
+    position:fixed;
+    bottom: 0;
+    text-align: center;
+}
+.sortCheckbox{
+    height: 14px;
+    width: 14px;
+    margin-top: 18px;
+}
+.footer label{
+    font-size: 15px;
+}
 </style>
